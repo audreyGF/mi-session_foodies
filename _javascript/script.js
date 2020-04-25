0e9862e301e41928afe542a07e4271c7e2305509
Audrey Gauthier <1837491@cstj.qc.ca>
2020-04-25 13:54:20 -0400

    Menu mobile script amélioration (js)

--- _javascript/script.js
+++ _javascript/script.js
@@ -4,13 +4,29 @@
 //                     _script.js                     //
 //----------------------------------------------------//
 
-$(function () {
-    $('.menu-bars').click(function () {
-        $('.nav-wrapper').toggleClass('nav-close');
-    });
 
-    $('.nav-item').click(function () {
-        $('.nav-wrapper').toggleClass('nav-close');
-    });
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Permet d'animer (ouverture et fermeture) le panneau du 
+ * menu mobile
+ * 
+ */
+function toggleNav(p){
+    $(p).toggleClass('nav-close');
+    $(p).toggleClass('nav-open');
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Gestion des évènements d'interaction
+ * 
+ */
+$(function(){
+    if($('.nav-wrapper').hasClass('estNav')){
+        $('.menu-bars').on('focus', function(){
+            toggleNav('.nav-wrapper');
+        });
+        $('.menu-bars').on('blur', function(){
+            toggleNav('.nav-wrapper');
+            $(this).removeAttr('style');
+        });
+    } 
 });
-

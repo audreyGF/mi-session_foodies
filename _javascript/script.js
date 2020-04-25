9a7d6d8d17028fedfc6373b4ca5c97d060068fbb
Audrey Gauthier <1837491@cstj.qc.ca>
2020-04-25 13:33:24 -0400

    Reclassement dossiers et déplacement javascript (menu-mobile)

---
+++ _javascript/script.js
@@ -0,0 +1,16 @@
+//----------------------------------------------------//
+//                                                    //
+//                     // SCRIPT //                   //
+//                     _script.js                     //
+//----------------------------------------------------//
+
+$(function () {
+    $('.menu-bars').click(function () {
+        $('.nav-wrapper').toggleClass('nav-close');
+    });
+
+    $('.nav-item').click(function () {
+        $('.nav-wrapper').toggleClass('nav-close');
+    });
+});
+
